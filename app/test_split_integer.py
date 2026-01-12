c82feee2ab18ecea95b3d388d58b9d8863740c9c
Vadym Kravchenko <vadymkravchenko.us@gmail.com>
2026-01-12 18:20:06 -0500

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,7 +5,6 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
 
 
-
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 3) == [2, 2, 2]
     assert split_integer(20, 4) == [5, 5, 5, 5]
